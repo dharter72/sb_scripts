a1524b55a4de8db0e49a9457316a458df1f91154
Doug Harter <dharter@ancestry.com>
2016-08-01 17:06:07 -0600

    Committed LOHP.java.

---
+++ LOHP.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class LOHP {
+    public static void main(String[] args) throws Exception {
+        FirefoxDriver wd;
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://www.ancestry.com/");
+        wd.findElement(By.linkText("Explore with AncestryDNA")).click();
+        if (!wd.findElement(By.tagName("html")).getText().contains("$89")) {
+            System.out.println("verifyTextPresent failed");
+        }
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
